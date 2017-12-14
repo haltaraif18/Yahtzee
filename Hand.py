fcd4990bd217be194756dc459ebcab30997470c6
Faisal <falsaif18@choate.edu>
2017-12-14 14:46:11 -0500

    methods

--- Hand.py
+++ Hand.py
@@ -1,2 +1,24 @@
 class hand:
 	def __init__(self):
+
+
+
+#This method allows the player to select what dices they want to keep so they can roll the other dices to roll in order to get a better score. 
+#Sends the information to hand.roll.
+	def selectDice(): 
+
+
+
+#Arguably the most important method of them all. This method keeps helps the player select a category such as full house, ones or twos etc. 
+#Then it sends the information ot player.points and player.category so score adjustment can be made. Sub-functions could help here.
+	def selectCategory():
+
+
+
+
+#This class has 5 dices. Numbered 1 through five. This method rolls 5 dices on the first roll, then receives information from hand.selectDice 
+#to know what dices to roll on the subsequent rolls.
+	def roll():
+
+
+
