763ff77fa9cb88c40669c29b6d295c5c88bc7018
Faisal <falsaif18@choate.edu>
2017-12-14 14:48:07 -0500

    player

--- player.py
+++ player.py
@@ -1,3 +1,17 @@
 class player:
 	def __init__(self, score):
 		
+
+
+#This method keeps track of each players' points. Needs method hand.selectCategory to know what category was selected by the player; 
+#then, player.points adds up the points and keeps track of them till the end of the game.
+	def points():
+
+
+
+
+#This method keeps track of the categories that a player used so that he cannot use the same category more than one time. 
+#It needs hand.selectCategory to know and keep track of the categories that were used.
+	def category():
+
+
