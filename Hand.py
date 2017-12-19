fb1a7e177e90fda07ddcd5633018014190c3bbc7
Faisal <falsaif18@choate.edu>
2017-12-19 15:42:27 -0500

    lower section

--- Hand.py
+++ Hand.py
@@ -16,8 +16,8 @@ class hand:
 #This method allows the player to select what dices they want to keep so they can roll the other dices to roll in order to get a better score. 
 #Sends the information to hand.roll.
 	def selectDice(self, list): 
-		hand = input('select the dice position you would like to roll again. Do not select the dice you want to keep: ')
-		hand = list.split(",")
+		hand = input("select the dice position you would like to roll again. Do not select the dice you want to keep: ")
+		hand = hand.split(",")
 		for i in hand:
 			intversion = int(i)
 			die[intversion-1] = random.randrange(1,7)
@@ -37,13 +37,96 @@ class hand:
 			upperSection(3)
 		elif choice == "twos":
 			upperSection(2)
-		elif choice == "ones":
+		elif choice == "aces":
 			upperSection(1)
+		elif choice == "Three of a kind"
+			ThreeOfKind()
+		elif choice == "Four of a kind"
+			FourOfKind()
+		elif choice == "Full House"
+			FullHouse()
+		elif choice == "Small Straight"
+			smallStraight()
+		elif choice == "Large Straight"
+			largeStraight()
+		elif choice =="Yahtzee"
+			yahtzee()
 
 	def upperSection(self, value):
-		num = list.count (value)
+		num = hand.count (value)
 		tempScore = num * value
 		overallScore += num * value
+
+	def ThreeOfKind(self, value):
+		sorted(die)
+		if (hand[0]==hand[1] and hand[2]==hand[1])
+		or (hand[1]==hand[2] and hand[3]==hand[2])
+		or (hand[2]==hand[3] and hand[4]==hand[3]):
+			num = hand.count(value)
+			tempScore = num * value
+			overallScore = num * value
+		else:
+			num = hand.count(value)
+			tempScore = num * value
+			overallScore = num * value
+
+	def FourOfKind(self, value):
+		sorted(die)
+		if (hand[0]==hand[1] and hand[2]==hand[1] and hand[3]==hand[2])
+		or (hand[1]==hand[2] and hand[3]==hand[2] and hand[4]==hand[3]):
+			num = hand.count(value)
+			tempScore = num * value
+			overallScore = num * value
+
+	def FullHouse(self):
+		sorted(die)
+		if ((hand[0]==hand[1] and hand[2]==hand[1])
+		and(hand[3]==hand[4])) 
+		or ((hand[0]==hand[1])
+		and (hand[2]==hand[3] and hand [4]==hand[3])):
+			tempScore = 25 
+		else:
+			num = hand.count(value)
+			tempScore = num * value
+			overallScore = num * value
+
+	def smallStraight(self):
+		sorted(die)
+		if ((hand[0]==1 and hand[1]==2 and hand[2]==3 hand[3]==4)
+		or (hand[1]==2 and hand[2]==3 and hand[3]==4 and hand[4]==5)
+		or (hand[0]==2 and hand[1]==3 and hand[2]==4 and hand[3]==5)
+		or (hand[1]==3 and hand[2]==4 and hand[3]==5 and hand[4]==6)):
+			tempScore = 30
+		else:
+			num = hand.count(value)
+			tempScore = num * value
+			overallScore = num * value
+
+	def largeStraight(self):
+		sorted(die)
+		if ((hand[0]==1 and hand[1]==2 and hand[2]==3 and hand[3]==4 and hand[4]==5)
+		or (hand[0]==2 and hand[1]==3 and hand[2]==4 and hand[3]==5 and hand[4]==6)):
+			tempScore = 40
+		else:
+			num = hand.count(value)
+			tempScore = num * value
+			overallScore = num * value
+
+	def yahtzee(self):
+		if hand[0]==hand[1] and hand[2]==hand[1] and hand[3]==hand[2] and hand[4]==hand[3]:
+			tempScore = 50
+		else:
+			num = hand.count(value)
+			tempScore = num * value
+			overallScore = num * value
+
+	def chance(self):
+		sum(hand)
+
+
+
+
+
 
 
 
